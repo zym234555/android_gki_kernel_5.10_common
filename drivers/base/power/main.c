bc11a5109c538611f5aa0fe6cbc968f7db62e810
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-10-22 18:04:02 +0200

    PM: suspend: Do not pause cpuidle in the suspend-to-idle path
    It is pointless to pause cpuidle in the suspend-to-idle path,
    because it is going to be resumed in the same path later and
    pausing it does not serve any particular purpose in that case.
    
    Rework the code to avoid doing that.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -882,6 +882,7 @@ void dpm_resume_early(pm_message_t state)
 void dpm_resume_start(pm_message_t state)
 {
 	dpm_resume_noirq(state);
+	cpuidle_resume();
 	dpm_resume_early(state);
 }
 EXPORT_SYMBOL_GPL(dpm_resume_start);
@@ -1542,9 +1543,13 @@ int dpm_suspend_end(pm_message_t state)
 	if (error)
 		goto out;
 
+	cpuidle_pause();
+
 	error = dpm_suspend_noirq(state);
-	if (error)
+	if (error) {
+		cpuidle_resume();
 		dpm_resume_early(resume_event(state));
+	}
 
 out:
 	dpm_show_time(starttime, state, error, "end");
